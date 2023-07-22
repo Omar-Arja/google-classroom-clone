a8ab9edf656edb11e73356b2fe5dce3496105300
Omar-Arja <omaralarja17@gmail.com>
2023-07-22 22:04:18 +0300

    return more info after receiving email

--- backend/api/email.php
+++ backend/api/email.php
@@ -3,12 +3,11 @@ include('connection.php');
 
 $email = $_POST['email'];
 
-$query = $mysqli->prepare('select email from users where email=?');
+$query = $mysqli->prepare('select first_name, last_name, email from users where email=?');
 $query->bind_param('s', $email);
 $query->execute();
-
 $query->store_result();
-$query->bind_result($email);
+$query->bind_result($first_name, $last_name, $email);
 $query->fetch();
 
 $num_rows = $query->num_rows();
@@ -16,5 +15,8 @@ if ($num_rows == 0) {
     $response['status'] = 'this email does not exist';
 } else {
     $response['status'] = 'email found';
+    $response['email'] = $email;
+    $response['first_name'] = $first_name;
+    $response['last_name'] = $last_name;
 }
 echo json_encode($response);
