5b6ac3c2d33469bc66e9571d21f088daa668ad96
Jaafar Mortada <jaafar1899@outlook.com>
2023-07-22 15:10:04 +0300

    added signup fetch

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -1,9 +1,50 @@
-const email = document.getElementById("signinEmail")
-const password = document.getElementById("signin-email")
-const next = document.getElementById("next")
+const pages = {}
 
-console.log(email,password,next)
+pages.base_url = 'http://localhost/google-classroom-clone/backend/api/'
 
-
-console.log('hi')
-console.log("helloworld")
+pages.myFetchSignup = () => {
+        const signup_btn = document.getElementById('signup-btn')
+        signup_btn.addEventListener('click', (e) =>{
+        e.preventDefault()
+        const first_name = document.getElementById('first-name-input')
+        const last_name = document.getElementById('last-name-input')
+        const email = document.getElementById('email-input')
+        const password = document.getElementById('password-input')
+        const ver_password = document.getElementById('ver-pass-input')
+        
+        const role = document.getElementsByName('role')
+        let chosen_role = ''
+        
+        for (i = 0; i < role.length; i++) {
+            if (role[i].checked)
+                chosen_role = role[i].value
+        }
+        
+        const first_name_val = first_name.value
+        const last_name_val = last_name.value
+        const email_val = email.value
+        const password_val = password.value
+        const ver_password_val = ver_password.value
+        
+        if ((password_val == ver_password_val) && chosen_role && first_name_val
+            && last_name_val && email_val && password_val && ver_password_val){
+            
+            const signup_data = new FormData()
+            signup_data.append('first_name', first_name_val)
+            signup_data.append('last_name', last_name_val)
+            signup_data.append('email', email_val)
+            signup_data.append('password', password_val)
+            signup_data.append('role', chosen_role)
+            
+            fetch(pages.base_url + 'signup.php', {
+                method: 'POST',
+                body: signup_data,
+                // redirect: 'follow',
+            }).then(response => response.json())
+            .then(data => {
+                console.log(data.status)
+            }).catch(error => console.log(error))
+            
+        }
+    })
+}
