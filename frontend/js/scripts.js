79924529a8f2100896b55609d184db1fc222a9bc
Rayan Emad <rayanemad508@gmail.com>
2023-07-22 14:14:59 +0300

    login email js

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -1,9 +1,41 @@
-const email = document.getElementById("signinEmail")
-const password = document.getElementById("signin-email")
-const next = document.getElementById("next")
-
-console.log(email,password,next)
+const nextButton = document.getElementById("next");
 
 
-console.log('hi')
-console.log("helloworld")
+nextButton.addEventListener("click", function(e){
+    e.preventDefault();
+
+    let email = document.getElementById("signin-email").value;
+
+    // For example, using fetch:
+    fetch('http://localhost/FullStackMiniProject/signin.php', {
+        method: 'POST',
+        body: JSON.stringify({ 
+            email: email }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        handleResponse(data);
+    })
+    .catch(error => {
+        console.error('Error In Email API: ', error);
+    });
+});
+
+
+function handleResponse(data){
+    const response = data.status;
+    switch (response){
+        case 'this email does not exist':
+            break
+        case 'email found':
+            let email_tab = document.querySelector(".signin-email");
+            let password_tab = document.querySelector(".signin-password");
+
+            email_tab.style.display = "none";
+            password_tab.style.display = "flex";
+            break;
+
+        default : 
+            console.log("handleResponse Error")
+    }
+}
