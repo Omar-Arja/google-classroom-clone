a1c50d2c6039da02fecb9474f646289677be604b
Jaafar Mortada <jaafar1899@outlook.com>
2023-07-22 11:31:48 +0300

    fixed api

--- backend/api/email.php
+++ backend/api/email.php
@@ -3,7 +3,7 @@ include('connection.php');
 
 $email = $_POST['email'];
 
-$query = $mysqli->prepare('email from users where email=?');
+$query = $mysqli->prepare('select email from users where email=?');
 $query->bind_param('s', $email);
 $query->execute();
 
@@ -13,7 +13,7 @@ $query->fetch();
 
 $num_rows = $query->num_rows();
 if ($num_rows == 0) {
-    $response['status'] = "this email does not exist";
+    $response['status'] = 'this email does not exist';
 } else {
     $response['status'] = 'email found';
 }
