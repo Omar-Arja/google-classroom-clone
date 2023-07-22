b4486766f06fc93fc876c95b13f685f4bffb2391
Jaafar Mortada <jaafar1899@outlook.com>
2023-07-22 11:18:26 +0300

    fixed response and query

--- backend/api/email.php
+++ backend/api/email.php
@@ -3,24 +3,18 @@ include('connection.php');
 
 $email = $_POST['email'];
 
-$query = $mysqli->prepare('select user_id, first_name, last_name, email, role
-from users 
-where email=?');
+$query = $mysqli->prepare('email from users where email=?');
 $query->bind_param('s', $email);
 $query->execute();
 
 $query->store_result();
-$query->bind_result($user_id, $first_name, $last_name, $email, $role);
+$query->bind_result($email);
 $query->fetch();
 
 $num_rows = $query->num_rows();
 if ($num_rows == 0) {
     $response['status'] = "this email does not exist";
 } else {
-    $response['status'] = 'logged in';
-    $response['user_id'] = $user_id;
-    $response['first_name'] = $first_name;
-    $response['last_name'] = $last_name;
-    $response['role'] = $role;
+    $response['status'] = 'email found';
 }
 echo json_encode($response);
