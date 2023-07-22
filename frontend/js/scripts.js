2f4c479dd779e07c0b59ba1b24a08cb1776f32e3
Rayan Emad <rayanemad508@gmail.com>
2023-07-22 12:47:51 +0300

    added script

---
+++ frontend/js/scripts.js
@@ -0,0 +1,5 @@
+const email = document.getElementById("signinEmail")
+const password = document.getElementById("signin-email")
+const next = document.getElementById("next")
+
+console.log(email,password,next)
