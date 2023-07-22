6d6c28995272615351d680313c38c7c66ce42de1
Rayan Emad <rayanemad508@gmail.com>
2023-07-22 13:10:37 +0300

    added backend

--- backend/api/connection.php
+++ backend/api/connection.php
@@ -7,6 +7,6 @@
     $db_host = "localhost";
     $db_user = "root";
     $db_pass = null;
-    $db_name = "full_stack_mini_project_db";
+    $db_name = "google_classroom_db";
 
     $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
