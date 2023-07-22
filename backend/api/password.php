cf7cebc1a82ba3f9cb215d7d835ef95f39d7efb0
Omar-Arja <omaralarja17@gmail.com>
2023-07-22 16:32:26 +0300

    added handling for role in sign in and sign up

--- backend/api/password.php
+++ backend/api/password.php
@@ -1,11 +1,18 @@
 <?php
 include('connection.php');
 
+// map usertye_id to role
+$roleMapping = array(
+    1 => "teacher",
+    2 => "student"
+);
+
+
 $email = $_POST['email'];
 $password = $_POST['password'];
 
 $query = $mysqli->prepare('select user_id, first_name, last_name, email, password, role
-from users 
+from users
 where email=?');
 $query->bind_param('s', $email);
 $query->execute();
@@ -14,12 +21,14 @@ $query->store_result();
 $query->bind_result($user_id, $first_name, $last_name, $email, $hashed_password, $role);
 $query->fetch();
 
+
 if (password_verify($password, $hashed_password)) {
     $response['status'] = 'logged in';
     $response['user_id'] = $user_id;
     $response['first_name'] = $first_name;
     $response['last_name'] = $last_name;
     $response['email'] = $email;
+    $role = $roleMapping[$role];
     $response['role'] = $role;
 } else {
     $response['status'] = "wrong password";
--- backend/api/signup.php
+++ backend/api/signup.php
@@ -9,6 +9,14 @@ $password = $_POST['password'];
 $role = $_POST['role'];
 
 
+// map role to usertye_id
+$roleMapping = array(
+    "teacher" => 1,
+    "student" => 2
+);
+
+$role = $roleMapping[$role];
+
 $check_username = $mysqli->prepare('select email from users where email=?');
 $check_username->bind_param('s', $email);
 $check_username->execute();
@@ -18,7 +26,7 @@ $username_exists = $check_username->num_rows();
 if ($username_exists == 0) {
     $hashed_password = password_hash($password, PASSWORD_BCRYPT);
     $query = $mysqli->prepare('insert into users(first_name, last_name, email, password, role) values(?,?,?,?,?)');
-    $query->bind_param('sssss', $first_name, $last_name, $email, $hashed_password, $role);
+    $query->bind_param('ssssi', $first_name, $last_name, $email, $hashed_password, $role);
     $query->execute();
     $response['status'] = "signed up successfully";
 } else {
