65a37c77b084f2f648ac11713fb5dcacf204af9c
Jaafar Mortada <jaafar1899@outlook.com>
2023-07-23 22:33:06 +0300

    added php to upload files to submissions

--- backend/api/send_assignment.php
+++ backend/api/send_assignment.php
@@ -1,5 +1,11 @@
-
 <?php
-$response['status'] = "success";
-echo json_encode($response);
-?>
+include('connection.php');
+
+$currentDateTime = date('M d H:i');
+
+var_dump($_FILES);
+
+foreach($_FILES['submission']['tmp_name'] as $key => $value) {
+    move_uploaded_file($value, '../submissions/' .basename($_FILES['submission']['name'][$key]));
+
+}
