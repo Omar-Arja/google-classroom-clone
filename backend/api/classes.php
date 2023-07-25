7282b821dd5d4b0d243d6218a4256023bffc0806
Omar-Arja <omaralarja17@gmail.com>
2023-07-25 17:49:02 +0300

    added update for total number of students in a class

--- backend/api/classes.php
+++ backend/api/classes.php
@@ -9,6 +9,7 @@ $roleMapping = array(
 );
 
 
+// get all classes created by a user
 if (isset($_POST['user_id'])) {
     $user_id = $_POST['user_id'];
     $query = $mysqli->prepare('select class_id, class_name, class_section, class_subject, class_room, total_number_students, class_code, meet_link, usertype_id 
@@ -21,6 +22,24 @@ if (isset($_POST['user_id'])) {
     $classes = array();
 
     while ($query->fetch()) {
+
+        // calculate the total number of students in class
+        $enrollment_query = $mysqli->prepare('select count(*) from enrollements where class_id = ? and usertype_id = 2');
+        $enrollment_query->bind_param('i', $class_id);
+        $enrollment_query->execute();
+        $enrollment_query->store_result();
+        $enrollment_query->bind_result($total_students);
+        $enrollment_query->fetch();
+
+        // update the total number of students in the class
+        $update_total_query = $mysqli->prepare('update classes set total_number_students = ? where class_id = ?');
+        $update_total_query->bind_param('ii', $total_students, $class_id);
+        $update_total_query->execute();
+
+
+        // get the updated total number of students in the class
+        $total_number_students = $total_students;
+        
         $class = array(
             'class_id' => $class_id,
             'class_name' => $class_name,
@@ -35,6 +54,7 @@ if (isset($_POST['user_id'])) {
         $classes[] = $class;
     }
 
+    // get all classes enrolled by a user
     $enrollment_query = $mysqli->prepare('select c.class_id, c.class_name, c.class_section, c.class_subject, c.class_room, c.total_number_students, c.meet_link, e.usertype_id
     from classes c
     inner join enrollements e ON c.class_id = e.class_id
