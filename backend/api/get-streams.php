497604a0ffbfa7feb239329637e24f65f96502b0
Jaafar Mortada <jaafar1899@outlook.com>
2023-07-25 13:17:42 +0300

    bug fixes

--- backend/api/get-streams.php
+++ backend/api/get-streams.php
@@ -1,19 +1,19 @@
 <?php
 include('connection.php');
 
-$class_id = $_GET['id_class'];
+$class_id = $_POST['class_id'];
 
 $query = $mysqli->prepare('SELECT streams.stream_id, streams.class_id, streams.user_id,streams.content,streams.post_date,streams.number_of_likes,users.first_name,users.last_name	
                            FROM streams INNER JOIN users
                            ON streams.user_id = users.user_id
                            WHERE class_id=?
                            ORDER BY stream_id DESC');
-$query->bind_param('i', $class_id);
+$query->bind_param('s', $class_id);
 $query->execute();
 
 $query->store_result();
 $query->bind_result($stream_id, $class_id, $user_id,$content,$post_date,$number_of_likes,$first_name,$last_name);
-
+$streams = array();
 while($query->fetch()){
     $stream1 = array(
         'stream_id' => $stream_id,
