7654ccd66972b6c281d651908de563086ce6cd54
Jaafar Mortada <jaafar1899@outlook.com>
2023-07-26 05:08:52 +0300

    fixed typo

--- backend/api/reset-password.php
+++ backend/api/reset-password.php
@@ -4,7 +4,7 @@ include('connection.php');
 $email_to_reset_pass = $_POST['email'];
 $new_password = $_POST['new_password'];
 
-$tokena = $_POST['token'];
+$token = $_POST['token'];
 
 $query = $mysqli->prepare('select user_id from users where email=?');
 $query->bind_param('s', $email_to_reset_pass);
@@ -14,8 +14,8 @@ $query->store_result();
 $query->bind_result($user_id);
 
 if($query->fetch()){
-    $query2 = $mysqli->prepare('select user_id, expiry from token where token = ?');
-    $query2->bind_param('i', $tokena);
+    $query2 = $mysqli->prepare('select user_id, expiry from tokens where token = ?');
+    $query2->bind_param('i', $token);
     $query2->execute();
     $query2->store_result();
     $query2->bind_result($user_id, $expiry);
@@ -26,15 +26,14 @@ if($query->fetch()){
             $query3 = $mysqli->prepare('update users set password = ? where email = ?');
             $hashed_new_password = password_hash($new_password, PASSWORD_BCRYPT);
             $query3->bind_param('ss', $hashed_new_password, $email_to_reset_pass);
-
-// Execute the UPDATE query
+            
                 if ($query3->execute()) {
                     $response['status'] = "password updated successfully";
                 } else {
                     $response['status'] =  "Error updating password: " . $mysqli->error;
                 }
             }
-        $query4 = $mysqli->prepare('delete from token where user_id = ?');
+        $query4 = $mysqli->prepare('delete from tokens where user_id = ?');
         $query4->bind_param('i', $user_id);
         $query4->execute();
             } else {
